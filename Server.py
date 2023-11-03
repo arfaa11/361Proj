c1ccde97a0208214038e579129a471422145a657
Arfaa Mumtaz <mumtazm@students.cs.macewan.ca>
2023-11-03 11:50:29 -0600

    create Server.py and Client.py initial files

---
+++ Server.py
@@ -0,0 +1,20 @@
+'''
+Student names: - Arfaa Mumtaz
+               - Cory Beaunoyer
+               - Kevin Esperida
+               - Olasubomi Badiru
+Instructor name: Mahdi Firoozjaei
+Assignment: Project
+Program name: Server.py
+Program purpose: <TODO>
+'''
+
+import json
+import socket
+import os
+import glob
+import datetime as dt
+import sys
+
+def server():
+    pass
