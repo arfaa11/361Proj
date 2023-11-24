802ca9f8f1fb8d2d1763a132796ca9b944bc44d6
arfaa11 <mumtazm@mymacewan.ca>
2023-11-24 15:16:15 -0700

    Unchanged; Moved to ClientMachine

---
+++ ClientMachine/Client_enhanced.py
@@ -0,0 +1,24 @@
+'''
+Student names: - Arfaa Mumtaz
+               - Cory Beaunoyer
+               - Kevin Esperida
+               - Olasubomi Badiru
+Instructor name: Mahdi Firoozjaei
+Assignment: Secure Mail Transfer Project
+Program name: Client_enhanced.py
+Program purpose: <TODO>
+'''
+
+import json
+import socket
+import os
+import glob
+import datetime as dt
+import sys
+
+def enhancedClient():
+    pass
+
+if __name__ == "__main__":
+    #enhancedClient()
+    pass
